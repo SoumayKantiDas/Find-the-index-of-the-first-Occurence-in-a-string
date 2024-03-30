ab39bac5866ba06f1ee2410f13f3462b1285dbd0
anikd <anikd@ANIK>
2024-03-30 15:24:05 +0600

    Add project files.

---
+++ Find the index of the first Occurence in a string/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Please Enter the haystack");
+        string haystack = Console.ReadLine();
+        Console.WriteLine("Please Enter the needle");
+        string needle = Console.ReadLine(); 
+        int i = StrStr(haystack, needle);
+        Console.WriteLine(i);
+
+    }
+    public static int StrStr(string haystack, string needle)
+    {
+        int haystacklen = haystack.Length;
+        int needlelen = needle.Length;
+        if (haystacklen < needlelen)
+        {
+
+            return -1;
+
+        }
+        if (needlelen == 0) 
+        {
+           return 0;
+        }
+        for(int i = 0; i <= haystacklen - needlelen; i++)
+        {
+            if(haystack.Substring(i, needle.Length) == needle) { return i; }
+        }
+
+        return -1; 
+
+    }
+}
